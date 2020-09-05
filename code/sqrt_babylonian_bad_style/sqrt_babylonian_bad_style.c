49abeb92bcfaebff6ef273d0109cb5a8867632a7
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2020-09-05 14:14:26 +0200

    update

--- code/sqrt_babylonian_bad_style/sqrt_babylonian_bad_style.c
+++ code/sqrt_babylonian_bad_style/sqrt_babylonian_bad_style.c
@@ -2,8 +2,8 @@
 
 double square_root(double s) {
 double xiter = s; int i;
-if (s == 0.0) return 0.0;
-for (i = 0; i < 10; i = i + 1) xiter = 0.5 * (xiter + s / xiter); return xiter;}
+if (s==0.0) return 0.0;
+for (i=0;i<10;i=i+1) xiter=0.5*(xiter+s/xiter); return xiter;}
 
 int main(void)
 {
