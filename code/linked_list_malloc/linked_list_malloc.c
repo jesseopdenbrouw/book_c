543ad326640d8de0d3cbf09e51bf85b6c3e46a16
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2020-10-21 10:02:42 +0200

    New example code

---
+++ code/linked_list_malloc/linked_list_malloc.c
@@ -0,0 +1,119 @@
+/* The includes needed */
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+/* Keep Visual Studio happy */
+#pragma warning(disable : 4996)
+
+/* The node structure */
+struct node {
+	int age;             /* The age */
+	char name[20];       /* The name */
+	struct node *next;   /* Pointer to the next */
+};
+
+/* Pointer to the first node, commonly known as the head */
+struct node *head = NULL;
+
+/* Function to create and fill a new node */
+struct node* create_node(int age, char name[]);
+/* Function to print all nodes */
+void print_all_nodes(void);
+/* Function to remove all nodes */
+void remove_all_nodes(void);
+
+int main(void) {
+
+	int age;
+	char name[20];
+
+	/* Read in the data */
+	do {
+		printf("Give age (> 0): ");
+		scanf("%d", &age);
+		if (age <= 0) { break; }
+		printf("Give name: ");
+		scanf("%s", name);
+
+		/* Create new node, bail out if error */
+		if (create_node(age, name) == NULL) {
+			printf("Node creation failed, bailing out!\n");
+			return -1;
+		}
+
+	} while (age > 0);
+
+	/* Print all nodes */
+	print_all_nodes();
+
+	/* Remove all nodes */
+	return 0;
+}
+
+/* Function to create and fill a new node */
+struct node *create_node(int age, char name[]) {
+
+	struct node *created, *current;
+
+	/* Create a new node */
+	created = (struct node*) malloc(sizeof(struct node));
+	
+	/* Could we create a new node? No, return signalling NULL */
+	if (created == NULL) {
+		return NULL;
+	}
+
+	/* At first, there are no nodes, so head points to NULL */
+	/* We test for it and set the head to the first node */
+	if (head == NULL) {
+		head = created;
+	}
+	else {
+		/* There are already nodes, so find the last one */
+		current = head;
+		while (current->next != NULL) {
+			current = current->next;
+		}
+
+		/* Let the last one point to the new node */
+		current->next = created;
+	}
+
+	/* Fill the new node with data */
+	created->age = age;
+	strcpy(created->name, name);
+	created->next = NULL;
+
+	/* Return address of new node */
+	return created;
+}
+
+/* Function to print all nodes in the list */
+void print_all_nodes(void) {
+
+	struct node *current;
+
+	/* Traverse all nodes */
+	current = head;
+	while (current != NULL) {
+		printf("Age: %2d, name: %s\n", current->age,
+			                                      current->name);
+		current = current->next;
+	}
+}
+
+/* Function to remove all nodes */
+void remove_all_nodes(void) {
+
+	struct node *remnode;
+
+	while (head != NULL) {
+		/* Make sure we point to the node to be deleted */
+		remnode = head;
+		/* Goto next in the list */
+		head = head->next;
+		/* Now we can safely remove the node */
+		free(remnode);
+	}
+}
---
+++ code/linked_list_simple/linked_list_simple.c
@@ -0,0 +1,38 @@
+/* Needed includes */
+#include <stdio.h>
+
+/* The nodes in the list*/
+struct node {
+	int age;
+	char name[20];
+	struct node *next;
+};
+
+int main(void) {
+
+	/* Populate the list */
+	struct node a = { 43, "Inge", NULL };
+	struct node b = { 18, "Jesscia", NULL };
+	struct node c = { 37, "Karin", NULL };
+	struct node d = { 14, "Janet", NULL };
+
+	/* Let each item point to the next */
+	a.next = &b;
+	b.next = &c;
+	c.next = &d;
+	/* Except for d, which points to NULL */
+
+	/* Variable to traverse the list */
+	/* At first, point to structure a */
+	struct node *current = &a;
+
+	/* Loop trough the complete list and print data */
+	while (current != NULL) {
+		printf("Age: %2d, name: %s\n", current->age,
+			                           current->name);
+        /* Point to the next in the list */
+		current = current->next;
+	}
+
+	return 0;
+}
