97e9df00ad447289303feb68f3eefa00ca6b5549
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2020-12-12 08:49:10 +0100

    update & remove

--- code/werkpunt_diode/werkpunt_diode.c
+++ code/werkpunt_diode/werkpunt_diode.c
@@ -1,12 +1,27 @@
+//
+// Find diode DC operation point using bisection method
+//
+// (c)2020, Jesse op den Brouw <J.E.J.opdenBrouw@hhs.nl>
+//
+
 #include <stdio.h>
 #include <math.h>
 
+// Tikz plot function for use in \LaTeX, for red led
 //	\addplot [domain=0:1.9,samples=201,linecolor1,thick,name path=dline] gnuplot[id=diode]{1e-21*(exp(x/(1.4*0.02569257028945))-1)};
 
+// Parameters for a typical red LED
+//double R = 220.0;
+//double U0 = 5.0;
+//double Is = 1e-24;
+//double n = 1.4;
+//double Ut = 0.02569257028945;
+
+// Paramaters for a typical silicon diode
 double R = 220.0;
-double U0 = 5.0;
-double Is = 1e-24;
-double n = 1.4;
+double U0 = 1.0;
+double Is = 1e-12;
+double n = 1.1;
 double Ut = 0.02569257028945;
 
 // Load line voltage source and series resistor
@@ -44,6 +59,7 @@ int main(void) {
 		printf("Ud: %.16lf\n", mid);
 	}
 
+	// Print final solution
 	printf("-----------------------\nUd: %.16lf\n", mid);
 	printf("Id: %.16lf\n", f(mid));
 }
