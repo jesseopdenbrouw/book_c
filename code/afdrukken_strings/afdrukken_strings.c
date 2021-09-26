7abcb26b7d92b17b5cbc212f96dadaecf25c2f9b
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2021-09-26 14:14:34 +0200

    new code examples

---
+++ code/afdrukken_strings/afdrukken_strings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char day[7][10] = { "zondag", "maandag", "dinsdag",
+                           "woensdag", "donderdag", "vrijdag",
+                           "zaterdag" };
+
+    int aantal = sizeof day / sizeof day[0]; /* bereken aantal strings */
+
+    for (int i = 0; i < aantal; i = i + 1)
+    {
+        printf("%s\n", day[i]);
+    }
+
+    return 0;
+}
---
+++ code/sizeof/sizeof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("sizeof char = %d\n", sizeof(char));
+    printf("sizeof int = %d\n", sizeof(int));
+    printf("sizeof long int = %d\n", sizeof(long int));
+    printf("sizeof float = %d\n", sizeof(float));
+    printf("sizeof double = %d\n", sizeof(double));
+
+    return 0;
+}
